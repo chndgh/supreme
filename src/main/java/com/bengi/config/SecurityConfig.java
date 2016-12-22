4506ea02f6be3b8efb3fe8ad2b0c2f2259658893
chndgh <chndgh@163.com>
2016-12-22 18:07:56 +0800

    added security

--- src/main/java/com/bengi/config/SecurityConfig.java
+++ src/main/java/com/bengi/config/SecurityConfig.java
@@ -34,6 +34,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
                 .antMatchers("/","/home").permitAll()
+                .antMatchers("/helloadmin").hasRole("admin")
                 .anyRequest().authenticated()
              .and()
                 .formLogin()
